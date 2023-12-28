e570cb4c18cbf6a34e328989a6250ed242648119
정우혁 <alalalalalalall367@gmail.com>
2023-12-28 14:59:57 +0900

    feat: Member, Team 엔티티 설계

---
+++ src/test/java/syudy/datajpa/entity/MemberTest.java
@@ -0,0 +1,53 @@
+package syudy.datajpa.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+
+        Team teamA = new Team("sktt1");
+        Team teamB = new Team("samsung");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("Faker", 26, teamA);
+        Member member2 = new Member("Owner", 25, teamA);
+        Member member3 = new Member("ShowMaker", 30, teamB);
+        Member member4 = new Member("Zeus", 33, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 초기화
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("member.team = " + member.getTeam());
+        }
+    }
+}
